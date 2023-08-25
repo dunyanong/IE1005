19d1cdbd8fdf00543229dbc1ccd91d0eebc014bf
Dun Yan <ong.dy2004@gmail.com>
2023-08-25 20:01:30 +0800

    series circuits

--- test.c
+++ test.c
@@ -2,15 +2,23 @@
 #include <math.h>
 
 int main(void) {
-    int X, x, sum;
-    
-    printf("Enter the value of X:");
-    scanf("%d", &X);
+    double V, I, R1, R2, R3;
 
-    printf("Enter the value of x:");
-    scanf("%d", &x);
-    sum = X + x;
-    printf("The sum of %d and %d is %d\n", X, x, sum);
+    printf("Enter the voltage:");
+    scanf("%lf", &V);
+
+    printf("Enter the R1:");
+    scanf("%lf", &R1);
+
+    printf("Enter the R2:");
+    scanf("%lf", &R2);
+
+    printf("Enter the R3:");
+    scanf("%lf", &R3);
+
+    I = V / (R1 + R2 + R3);
+
+    printf("The current is %lf \n", I);
     
     return 0;
 }
