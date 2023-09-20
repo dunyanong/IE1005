91ebb3fb57a7b7e01554e908d1138b1a5314b2fb
Dun Yan <ong.dy2004@gmail.com>
2023-09-20 23:59:27 +0800

    nestedLoop-exercise

--- test.c
+++ test.c
@@ -1,37 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-double calculate_e(int n) {
-    double result = 1.0;
-    double x = 1.0;
-
-    for (int i = 1; i < n; i++) {
-        // x value still remain after assigning
-        x *= 1.0 / i;
-
-        // printf("x: %lf, i: %d\n", x, i);
-
-        // confusion?
-        result += x;
-    }
-
-    return result;
-}
 
 int main() {
-    int n; // Number of terms in the Taylor series
-    printf("Enter the number of terms (n): ");
-    scanf("%d", &n);
+    int height, width;
 
-    double e_approximation = calculate_e(n);
-    printf("Approximation of e using %d terms: %.15f\n", n, e_approximation);
+    // Input height and width from the user
+    printf("Enter two integers for height and width: ");
+    scanf("%d %d", &height, &width);
+
+    // Iterate through each row (height)
+    for (int heightCounter = 0; heightCounter < height; heightCounter++) {
+        // Print the top and bottom lines
+        if (heightCounter == 0 || heightCounter == height - 1) {
+            for (int widthCounter = 0; widthCounter < width; widthCounter++) {
+                printf("=");
+            }
+            printf("\n");
+        } else {
+            // Print the left and right borders
+            printf("*");
+            for (int widthCounter = 2; widthCounter < width; widthCounter++) {
+                printf(" ");
+            }
+            printf("*\n");
+        }
+    }
 
     return 0;
 }
 
 
 
+// #include <stdlib.h>
+// #include <time.h>
 
+// int main() {
+//     int height, width;
 
+//     printf("Enter two integers for height and width: ");
+//     scanf("%d %d", &height, &width);
 
+//     int newHeight = height;
+//     int newWidth = width;
+    
+//     for (int heightCounter = 2; heightCounter < newHeight; heightCounter++) {
+//             if (heightCounter == 2) {
+//                 for (int widthCounter = 0; widthCounter < newWidth; widthCounter++) {
+//                     printf("=");
+//                 }
+//                 printf("\n");
+//             }
+
+//             if (heightCounter != height - 1 || heightCounter != 0) {
+//                 for (int k = 0; k < newWidth; k++) {
+//                     if (k == 0) {
+//                         printf("*");
+//                     } else if (k == newWidth - 1) {
+//                         printf("*\n");
+//                     } else {
+//                         printf(" ");
+//                     }
+//                 }
+//             }
+
+//             if (heightCounter == (height - 1)) {
+//                 for (int widthCounter = 0; widthCounter < newWidth; widthCounter++) {
+//                     printf("=");
+//                 }
+//                 printf("\n");
+//             }                          
+//     }
+//     printf("\n");
+// }
