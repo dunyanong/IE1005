8e72f88ec5c73ff1c15160d04e5f9e27733b7227
Dun Yan <ong.dy2004@gmail.com>
2023-10-03 17:20:55 +0800

    isTriangle

--- test.c
+++ test.c
@@ -3,25 +3,41 @@
 #include <ctype.h>
 #include <math.h>
 
-double repay(int n);
+int istriangle(double a, double b, double c);
+double areatri(double a, double b, double c);
 
 int main() {
-    // n = 12
-    double month = 12;
-    printf("repayment at the end of a year for MONTHLY case: %lf\n", repay(month));
+    double a, b, c;
+    printf("Enter a: ");
+    scanf("%lf", &a);
 
-    // n = 365
-    double day = 365;
-    printf("repayment at the end of a year for DAILY case: %lf\n", repay(day));
+    printf("Enter b: ");
+    scanf("%lf", &b);
+
+    printf("Enter c: ");
+    scanf("%lf", &c);  
+
+    if (istriangle(a, b, c) == 0) {
+        printf("Not a valid triangle\n");
+        return 1;
+    } else {
+        printf("Area of triangle: %lf\n", areatri(a, b, c));
+    }
 
     return 0;
 }
-double repay(int n) {
-    double sum = 1.0;
-    double P = 2.0;
-    for (double i = 1.0; i <= n; i++) {
-        sum = sum*(1 + i/n);
+int istriangle(double a, double b, double c)  {
+    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+        return 0;
     }
 
-    return sum;
+    return 1;
+}
+
+double areatri(double a, double b, double c) {
+    double s = (a + b + c) * 0.5;
+
+    double area = sqrt(s*(s-a) * (s-b) * (s-c));
+
+    return area;
 }
