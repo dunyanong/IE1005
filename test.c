1b706ae14081c23e7f2e6bff6833cf124c684dc4
Dun Yan <ong.dy2004@gmail.com>
2023-10-15 09:54:07 +0800

    scope-Exercise-5

--- test.c
+++ test.c
@@ -1,43 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <math.h>
+int main(void) {
+    int i;
+    for (i = 0; i < 5; i++) {
+        static int x = 10, y = 0;
+        printf("x = %d, y = %d\n", x, y);
 
-int istriangle(double a, double b, double c);
-double areatri(double a, double b, double c);
-
-int main() {
-    double a, b, c;
-    printf("Enter a: ");
-    scanf("%lf", &a);
-
-    printf("Enter b: ");
-    scanf("%lf", &b);
-
-    printf("Enter c: ");
-    scanf("%lf", &c);  
-
-    if (istriangle(a, b, c) == 0) {
-        printf("Not a valid triangle\n");
-        return 1;
-    } else {
-        printf("Area of triangle: %lf\n", areatri(a, b, c));
+        x++;
+        y++;
     }
-
     return 0;
-}
-int istriangle(double a, double b, double c)  {
-    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
-        return 0;
-    }
-
-    return 1;
-}
-
-double areatri(double a, double b, double c) {
-    double s = (a + b + c) * 0.5;
-
-    double area = sqrt(s*(s-a) * (s-b) * (s-c));
-
-    return area;
 }
